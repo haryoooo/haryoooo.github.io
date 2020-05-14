63bcc6604dad44e9995752157e8b7fe60bad969a
unknown <DIGITAL@ISG-DIGITAL.innograph.com>
2020-05-14 14:17:43 +0700

    Menambahkan Animation h1 di Jumbotron

--- JS/Biography.js
+++ JS/Biography.js
@@ -22,6 +22,42 @@ function topFunction() {
 
 /* End of Scroll Top */
 
+/* Animation H1 Jumbotron */
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+/* End of Animation H1 Jumbotron */
 
 /* Lightbox Gallery */
 
@@ -67,3 +103,4 @@ function showSlides(n) {
 }
 
 /* End Of Lightbox Gallery */
+
