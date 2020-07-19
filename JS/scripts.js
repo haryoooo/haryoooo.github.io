d0cee7973c0bc101fb22855f56cc9cd423bc45c6
unknown <DIGITAL@ISG-DIGITAL.innograph.com>
2020-07-19 10:18:12 +0700

    Update Responsive Page

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,3 @@
-
 /* Scroll Top */
 
 //Get the button
@@ -74,12 +73,13 @@ function showSlides(n) {
 /* Animation On Scroll */
 
 AOS.init({
-  offset: 400, // offset (in px) from the original trigger point
+  offset: 600, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000 // values from 0 to 3000, with step 50ms
 });
 
 /* End of Animation On Scroll */
+
 
 // Skill Slider 
 
